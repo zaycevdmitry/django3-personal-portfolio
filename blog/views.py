5f86b4a4eeace01a94e1dfebe7f5a2cd0de0c2d2
ZaycevDa <zaycev.dmitry.95@gmail.com>
2022-11-14 12:01:42 +0300

    All 46 lessons

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Blog
+
+def all_blogs(request):
+    blogs = Blog.objects.order_by("-date")[:2]
+    return render(request, "blog/all_blogs.html", {"blogs" : blogs})
